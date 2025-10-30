f0a1ea7eeef9ecece81fa8b91580e486794e58b3
Daniel Neville <st20305017@outlook.cardiffmet.ac.uk>
2025-10-30 17:47:33 +0000

    Workshop 5 practice

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -18,7 +18,24 @@ int main(void) {
 	//
 	// Setup game scene objects here
 	//
+	const float pi = 3.142f;
 	
+	addObject("player1", glm::vec2(1.0f, 1.0f), glm::radians(45.0f), glm::vec2(0.5f, 1.0f), "Resources\\Textures\\player1_ship.png", TextureProperties::NearestFilterTexture());
+	
+	GameObject2D* player1Object = getObject("player1");
+	if (player1Object != nullptr) {
+
+		// update player1 here
+
+	}
+	
+	addObject("player2");
+	
+
+
+
+
+
 
 	// Enter main loop - this handles update and render calls
 	engineMainLoop();
