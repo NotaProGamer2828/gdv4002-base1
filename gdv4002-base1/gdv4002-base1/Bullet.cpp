5b0a9be6d2015e3cd25d112f47ca5d3c756eb641
NotaProGamer2828 <st20305017@outlook.cardiffmet.ac.uk>
2026-01-10 20:07:20 +0000

    slight alteration to make pendingDelete work correctly

--- gdv4002-base1/gdv4002-base1/Bullet.cpp
+++ gdv4002-base1/gdv4002-base1/Bullet.cpp
@@ -1,12 +1,11 @@
-
 #include "Bullet.h"
 #include "Engine.h"
 
-Bullet::Bullet(glm::vec2 initPosition,float initOrientation,glm::vec2 initSize,GLuint initTextureID,glm::vec2 initDirection): GameObject2D(initPosition, initOrientation, initSize, initTextureID) 
+Bullet::Bullet(glm::vec2 initPosition,float initOrientation,glm::vec2 initSize,GLuint initTextureID,glm::vec2 initDirection)
+    : GameObject2D(initPosition, initOrientation, initSize, initTextureID)
 {
     Direction = initDirection;
 }
-bool pendingDelete = false; // This initally keeps the bullet from being deleted initally from player
 
 void Bullet::update(double tDelta)
 {
@@ -18,12 +17,12 @@ void Bullet::update(double tDelta)
     float halfW = getViewplaneWidth() / 2.0f;
 
     // Mark for deletion if bullet is off screen
-    if (!pendingDelete &&     // stops the print from being repeated continuously causing a potential crash or performance issue if program runs long enough
+    if (!this->pendingDelete &&
         (position.x < -halfW || position.x > halfW ||
          position.y < -halfH || position.y > halfH))
     {
-        pendingDelete = true; // bullet is deleted
-        printf("Bullet is deleted\n");  
+        this->pendingDelete = true;
+        printf("Bullet is deleted\n");
     }
 }
 
